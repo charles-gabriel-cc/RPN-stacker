31181491b50da2f80c699c0226a6ae90924e7ba3
Charles Gabriel <48693857+charles-gabriel-cc@users.noreply.github.com>
2021-11-26 13:59:28 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,39 +3,67 @@ import java.util.*;
 
 class Main {
   public static void main(String[] args) {
+    ArrayList<Token> tokens = new ArrayList<Token>();
+
     try {
       File myObj = new File("Calc1.stk");
       Scanner myReader = new Scanner(myObj);
-      Stack<Integer> rpn_stack = new Stack<>();
 
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
+        TokenType aux;
+
         if(data.equals("+")){
-          Integer a = rpn_stack.pop();
-          Integer b = rpn_stack.pop();
-          rpn_stack.push(b + a);
+          aux = TokenType.PLUS;
         } else if(data.equals("-")) {
-          Integer a = rpn_stack.pop();
-          Integer b = rpn_stack.pop();
-          rpn_stack.push(b - a);
+          aux = TokenType.MINUS;
         } else if(data.equals("*")) {
-          Integer a = rpn_stack.pop();
-          Integer b = rpn_stack.pop();
-          rpn_stack.push(b * a);
+          aux = TokenType.STAR;
         } else if(data.equals("/")) {
-          Integer a = rpn_stack.pop();
-          Integer b = rpn_stack.pop();
-          rpn_stack.push(b / a);
+          aux = TokenType.SLASH;
         } else {
-          rpn_stack.push(Integer.parseInt(data));
+          try {
+            Integer.parseInt(data);
+          } catch (NumberFormatException e){
+            System.out.println("Error: Unexpected character: " + data);
+            System.exit(0);
+          }
+          aux = TokenType.NUM;
         }
+        Token token = new Token(aux, data);
+        tokens.add(token);
+        System.out.println(token.toString());
       }
-      Integer result = rpn_stack.pop();
-      System.out.println("Saida: "+ result);
       myReader.close();
-    } catch (FileNotFoundException e) {
+    } catch (FileNotFoundException e){
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
+
+    Stack<Integer> rpn_stack = new Stack<>();
+
+    for (int i = 0; i < tokens.size(); i++) {
+      if (tokens.get(i).type == TokenType.PLUS) {
+        Integer a = rpn_stack.pop();
+        Integer b = rpn_stack.pop();
+        rpn_stack.push(b + a);
+      } else if (tokens.get(i).type == TokenType.MINUS) {
+        Integer a = rpn_stack.pop();
+        Integer b = rpn_stack.pop();
+        rpn_stack.push(b - a);
+      } else if (tokens.get(i).type == TokenType.STAR) {
+        Integer a = rpn_stack.pop();
+        Integer b = rpn_stack.pop();
+        rpn_stack.push(b * a);
+      } else if (tokens.get(i).type == TokenType.SLASH) {
+        Integer a = rpn_stack.pop();
+        Integer b = rpn_stack.pop();
+        rpn_stack.push(b / a);
+      } else {
+        rpn_stack.push(Integer.parseInt(tokens.get(i).lexeme));
+      }
+    }
+    Integer result = rpn_stack.pop();
+    System.out.println("Saida: "+ result);
   }
 }
